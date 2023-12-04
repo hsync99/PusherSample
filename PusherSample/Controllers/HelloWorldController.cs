1ac7fda1c14c53cc35a4fa3f9715eea4a450785d
Leonid Gavrilov <g.leonid@dogovor24.kz>
2023-12-04 22:48:46 +0600

    Добавьте файлы проекта.

---
+++ PusherSample/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PusherServer;
+using System.Net;
+
+namespace PusherSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult> HelloWorld()
+        {
+            var options = new PusherOptions
+            {
+                Cluster = "eu",
+                Encrypted = true
+            };
+
+            var pusher = new Pusher(
+              "1719290",
+              "f2b973891577862a5c42",
+              "25d89d448bc7435ae332",
+              options);
+
+            var result = await pusher.TriggerAsync(
+              "my-channel",
+              "my-event",
+              new { message = "hello world" });
+
+            return Ok();
+        }
+    }
+}
